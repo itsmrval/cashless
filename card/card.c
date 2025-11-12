357bc7eed807aaf29d67ac4c1783094aeec8c236
Valentin PUCCETTI <contact@valentinp.fr>
2025-11-12 15:01:26 +0100

    feat(card) v0.0.2

--- card/card.c
+++ card/card.c
@@ -12,7 +12,7 @@ uint8_t sw1, sw2;
 #define SIZE_ATR 8
 const char atr_str[SIZE_ATR] PROGMEM = "cashless";
 
-#define CARD_VERSION 1
+#define CARD_VERSION 2
 #define SIZE_CARD_ID 24
 #define SIZE_PIN 4
 #define EEPROM_PIN_ADDR 0
