8e86627cd4d1e62bd5e16437caae54582efe0739
Valentin PUCCETTI <contact@valentinp.fr>
2025-11-14 10:59:10 +0100

    feat(card) crypto

--- card/crypto/hexdigit_tab.h
+++ card/crypto/hexdigit_tab.h
@@ -2,10 +2,7 @@
 #define HEXDIGIT_TAB_H
 
 #include <avr/pgmspace.h>
-#include "bigint.h"
 
 const char hexdigit_tab_lc_P[] PROGMEM = "0123456789abcdef";
-
-static inline void bigint_print_hex(const bigint_t *a) { (void)a; }
 
 #endif
