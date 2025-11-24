0a8e6da6bec4d82cc75acdd8967105979335e911
Valentin PUCCETTI <contact@valentinp.fr>
2025-11-24 15:23:04 +0100

    feat(api) using public endpoint

--- front/src/api.js
+++ front/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3002';
+const API_BASE_URL = 'https://api.cashless.valentinp.fr/v1';
 
 export const api = {
   // Authentication
--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3002';
+const API_BASE_URL = 'https://api.cashless.valentinp.fr';
 
 export const api = {
   // Authentication
