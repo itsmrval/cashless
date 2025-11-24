03360f49021f937df6848908b1ba2e7d9698698c
Valentin PUCCETTI <contact@valentinp.fr>
2025-11-24 14:01:45 +0100

    .

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,8 +6,9 @@ const { verifyJWT } = require('../middleware/auth');
 router.post('/login', userController.login);
 router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
+router.get('/:id/balance', userController.getUserBalance);
 router.get('/:id', userController.getUser);
-router.post('/:id', userController.updateUser);
+router.patch('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
